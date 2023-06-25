356b4d2bc0f874f61ad1cec3132efb8a99948731
Noé Zúñiga <noe.zm2000@gmail.com>
2023-06-24 21:58:28 -0600

    Se agrega panel para administrar usuarios

--- Caseta/Cobro/cobroFO.py
+++ Caseta/Cobro/cobroFO.py
@@ -36,7 +36,7 @@ TipoPromocion = 1
 ###-###
 p = Usb(0x04b8, 0x0202, 0)
 penalizacion_con_importe = False
-
+from view_login import View_Login
 
 class FormularioOperacion:
 	def __init__(self):
@@ -1014,6 +1014,18 @@ class FormularioOperacion:
 		self.boton6=tk.Button(self.labelframe5, text="Reporte de Corte", command=self.Reporte_Corte, width=15, height=1, anchor="center", background="red")
 		self.boton6.grid(column=3, row=2, padx=4, pady=4)       
 
+
+		self.seccion_boton_usuario = ttk.LabelFrame(self.pagina3, text='Administrar usuarios')
+		self.seccion_boton_usuario.grid(row=3, column=1, padx=10, pady=10)
+
+		self.boton_usuarios=tk.Button(self.seccion_boton_usuario, text="Entrar",	 
+		command=lambda:{
+				self.desactivar(),
+				View_Login(),
+				self.activar()
+				},
+		width=15, height=1, anchor="center", background="red")
+		self.boton_usuarios.grid(column=0, row=0, padx=4, pady=4)  
 
 	def BoletoDentro2(self):
 		respuesta=self.operacion1.Autos_dentro()
@@ -2529,6 +2541,31 @@ class FormularioOperacion:
 		print("Se abre barrera\n")
 		print('------------------------------\n')
 
+	def desactivar(self):
+		"""
+		Desactiva los botones de la interface
+	
+		:param None: 
+
+		:raises None: 
+
+		:return:
+			- None
+		"""
+		self.ventana1.withdraw()  # oculta la ventana
+
+	def activar(self):
+		"""
+		Activa los botones de la interface
+
+		:param None: 
+
+		:raises None: 
+
+		:return:
+			- None
+		"""
+		self.ventana1.deiconify()
 
 
 #aplicacion1=FormularioOperacion()
